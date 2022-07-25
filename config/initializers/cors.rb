b24b36238a7f996e6fbaae856f4369702b5001f3
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-07-25 18:48:40 +0300

    set valid links to cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,8 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   case ENV.fetch("RAILS_ENV") 
   when "development" then create_cors_headers_for origin_url:   "https://localhost:4000", 
                                                   resource_url: "https://localhost:3000"
-  when "production" then  create_cors_headers_for origin_url:   "https://localhost:4000", 
-                                                  resource_url: "https://localhost:3000"
+  when "production" then  create_cors_headers_for origin_url:   "https://tauest-cli.herokuapp.com", 
+                                                  resource_url: "https://tauest-srv.herokuapp.com"
   else
     raise 'problems'
   end    
