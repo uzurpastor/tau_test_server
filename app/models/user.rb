4506b69c64a44f1020e87874b22efa5debb500d4
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-01 01:59:23 +0300

    spec the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,24 @@
 class User < ApplicationRecord
-  # before_save 
-  
+  #====== Callback =====#
+  before_validation :value_in_downcase, on: :create
+
   #====== Validations =====#
   validates_presence_of :name, :email, :password, :password_confirmation
   validates_uniqueness_of :email
   
   # Get regex from https://api.rubyonrails.org/classes/ActiveModel/Validations/HelperMethods.html#method-i-validates_format_of
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates_format_of :name, with: /\A\w+\z/, on: :create
 
-  validates_length_of :name, in: 2..40
+  validates_length_of :name, in: 2..16
   validates_length_of :password, in: 6..32
 
   has_secure_password
 
+  private
+
+  def value_in_downcase
+    self.email.downcase!
+    self.name.downcase!    
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without a title"
-  it "is not valid without a description"
-  it "is not valid without a start_date"
-  it "is not valid without a end_date"
+  let (:user) { described_class.new name: "username", 
+                                    email: "useremail.valid@mail.net",
+                                    password: "strongpass",
+                                    password_confirmation: "strongpass" }
+  describe "callback" do
+    it "saves with name in uppercase" do
+          user.name = "USERNAME"
+          user.save
+          expect(user.name).to eq("username")  
+        end
+    it "saves with email in uppercase" do
+          user.email = "UserEmail.Valid@Mail.Net"
+          user.save
+          expect(user.email).to eq("useremail.valid@mail.net")
+        end
+  end
+
+  describe 'validation of' do
+    describe 'email' do
+      it 'has not valid value' do
+            user.email = "useremail"
+            expect(user).to_not be_valid
+          end
+      it 'has repeatable value' do
+            duplicate = user.dup
+            duplicate.save
+            user.save
+            error = user.errors.full_messages_for(:email)
+            expect(error.to_sentence).to eq("Email has already been taken")
+          end
+    end
+
+    describe 'name' do
+      it 'has not valid value ' do
+            user.name = "user name"
+            expect(user).to_not be_valid
+          end
+      it 'has value that out of require range' do
+            user.name = "a"*18
+            user.save
+            error = user.errors.full_messages_for :name
+            expect(error.to_sentence).to match /Name is too long/
+          end
+    end
+    
+    describe 'password' do
+      it 'has too long value' do
+            user.name = "a"*33
+            user.save
+            error = user.errors.full_messages_for :name
+            expect(error.to_sentence).to match /Name is too long/
+          end
+      it 'has not match passwords' do
+            user.password_confirmation = "simplepass"
+            expect(user).to_not be_valid
+          end
+      it 'saves valid md5 hash to db' do
+            user.save
+            expect(user.password_digest.size).to eq(60)
+          end
+    end
+  end
 end
