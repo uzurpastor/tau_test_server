292061f0eeed611444f53fa74636a50d3ca3d2fe
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-07-31 13:36:23 +0300

    refactor puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,16 +2,21 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-port ENV.fetch("PORT") { 3030 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+if ENV['RAILS_ENV'] == 'production'
+  port        ENV.fetch("PORT")
+  environment ENV.fetch("RAILS_ENV")
+  pidfile     ENV.fetch("PIDFILE")
+end
 
-plugin :tmp_restart
 
 if ENV['RAILS_ENV'] == 'development'
   localhost_key = "#{File.join('config', 'local-cert', 'localhost-key.pem')}" 
   localhost_crt = "#{File.join('config', 'local-cert', 'localhost.pem')}"
+
+  worker_timeout  3600
+  port            3030
+  environment     "development"
+  pidfile         "tmp/pids/server.pid"
 
   ssl_bind 'localhost', 3000, {
     key: localhost_key,
@@ -19,3 +24,5 @@ if ENV['RAILS_ENV'] == 'development'
     verify_mode: 'none'
   }
 end
+
+plugin :tmp_restart
