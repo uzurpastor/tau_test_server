f44a278ef55bffd5118e367b04f56c4b0195839e
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-07-31 12:55:44 +0300

    change cors: get client url from another source

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,21 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
-  case ENV.fetch("RAILS_ENV") 
-  when "development" then create_cors_headers_for origin_url:   "https://localhost:4000", 
-                                                  resource_url: "https://localhost:3000"
-  when "production" then  create_cors_headers_for origin_url:   "https://tauest-cli.herokuapp.com", 
-                                                  resource_url: "https://tauest-srv.herokuapp.com"
-  else
-    raise 'problems'
-  end    
+  allow do
+    origins ENV.fetch("FRONTEND_URL")
 
-end
-  def create_cors_headers_for(origin_url:, resource_url:)
-    allow do
-        origins origin_url
-
-        resource resource_url,
-          headers: :any,
-          methods: [:get, :post]
-      end
+    resource "*",
+      headers: :any,
+      methods: [:get, :post]
   end
+end
