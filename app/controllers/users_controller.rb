1f373a8d03372aa3e2e74381bdbc9772d0325cf8
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-07-31 12:57:23 +0300

    create MVC for user [show, create, index] action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
-
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
   def create
-    debugger
     @user = User.new(user_params)
 
     if @user.save
@@ -25,8 +18,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     if @user.update(user_params)
       render :show, status: :ok, location: @user
@@ -35,20 +26,16 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  # before_save 
+  
+  #====== Validations =====#
+  validates_presence_of :name, :email, :password, :password_confirmation
+  validates_uniqueness_of :email
+  # Get regex from https://api.rubyonrails.org/classes/ActiveModel/Validations/HelperMethods.html#method-i-validates_format_of
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+
+  validates_length_of :name, in: 2..40
+  validates_length_of :password, in: 6..32
+
+  has_secure_password
+
 end
