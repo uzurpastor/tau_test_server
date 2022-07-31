b2b01ad1544436a36ecb0509d509ae31e849ba58
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-07-31 12:51:58 +0300

    edit user model schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_23_123854) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_160658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "table_users", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.boolean "is_author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
+    t.boolean "is_author", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
